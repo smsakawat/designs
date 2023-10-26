89dd2c3c05f37a2b749461a0f18cb1d8b6e4523c
Sakawat Hossain <sakawat.hossain.c@gmail.com>
2023-10-27 00:40:34 +0600

    added till testimonials section

--- fylo/tailwind.config.js
+++ fylo/tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         darkBlue1: "hsl(218, 28%, 13%)",
         darkBlue2: "hsl(216, 53%, 9%)",
         darkBlue3: "hsl(219, 30%, 18%)",
-        accentCyan: "#1dafab",
+        accentCyan: "hsl(176, 68%, 64%)",
         accentBlue: "hsl(198, 60%, 50%)",
         lightRed: "hsl(0, 100%, 63%)",
       },
