e591b6b6a711c4854d8e5ad2e8672ea2986366a0
smsakawat <sakawat.hossain.c@gmail.com>
2023-11-12 11:26:07 +0600

    nav and hero added

--- nikee/tailwind.config.js
+++ nikee/tailwind.config.js
@@ -1,28 +1,19 @@
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ["./*.html", "./*.{js,ts,jsx,tsx}"],
   theme: {
-    fontSize: {
-      xs: ["12px", "16px"],
-      sm: ["14px", "20px"],
-      base: ["16px", "19.5px"],
-      lg: ["18px", "21.94px"],
-      xl: ["20px", "24.38px"],
-      "2xl": ["24px", "29.26px"],
-      "3xl": ["28px", "50px"],
-      "4xl": ["48px", "58px"],
-      "8xl": ["96px", "106px"],
-    },
     extend: {
       fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
         palanquin: ["Palanquin", "sans-serif"],
-        montserrat: ["Montserrat", "sans-serif"],
       },
       colors: {
-        primary: "#ECEEFF",
-        "coral-red": "#FF6452",
+        primary: "#FF6452",
+        "primary-shade": "#e65a4a",
+        "primary-tint": "#ff7463",
         "slate-gray": "#6D6D6D",
-        "pale-blue": "#F5F6FF",
-        "white-400": "rgba(255, 255, 255, 0.80)",
+        "gray-one": "#ECEEFF",
+        "gray-two": "#F5F6FF",
+        "gray-three": "rgba(255, 255, 255, 0.80)",
       },
       boxShadow: {
         "3xl": "0 10px 40px rgba(0, 0, 0, 0.1)",
